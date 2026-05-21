7f2e2b9dc2d4f72fba9f573fe67bbeea675d243a
GetTheVigilante <getthevigilante@gmail.com>
2026-05-20 21:31:36 -0500

    Add NotFound.tsx to trigger redeploy
    Added a NotFound.tsx file to trigger Vercel redeploy.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,3 +12,7 @@ const Index: React.FC = () => {
 };
 
 export default Index;
+echo "// touch to trigger deploy" >> src/pages/NotFound.tsx
+git add src/pages/NotFound.tsx
+git commit -m "Trigger Vercel redeploy for NotFound"
+git push
